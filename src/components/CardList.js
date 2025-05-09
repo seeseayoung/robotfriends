1f9f679700caab23fc9240b6688e6af30b3feaeb
seeseayoung <seeseayoung@126.com>
2025-05-10 01:44:08 +0800

    error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,10 +3,7 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
 
-	if(true){
-		return Error('yyyyyyy');
-	}
-
+	
 	const CardComponent = robots.map((robot) => {
 		return <Card key={robot.name} id={robot.id} name={robot.name} email={robot.email} />
 	});
